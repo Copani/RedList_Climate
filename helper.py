3c772cf74d7f7734339a0277e3e3fbfbfad0a8b0
Claus Sarnighausen <claussar@pik-potsdam.de>
2024-07-23 14:58:29 +0200

    moved to gitLab

--- helper.py
+++
@@ -1,7 +0,0 @@
-import xarray as xr
-import numpy as np
-
-
-x = xr.open_dataset('../Data/ERA5/ERA5_temp_prec_wind_veg_1980_2023.nc')
-# save the precipitation variable as nc file
-x['tp'].to_netcdf('../Data/ERA5/ERA5_veg_1980_2023.nc')
